0fb80ec2250fdc690d3c9faa857625dcacf3535d
seonhyungjo <senjing9379@gmail.com>
2018-01-30 18:34:51 +0900

    update react page

--- copy-app/src/App.js
+++ copy-app/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './main/Main';
-import Nav from './commen/Nav.js';
-import Top from './commen/Top.js';
+
 
 class App extends Component {
 
-  constructor(state){
-    super(state);
-
+  state = {
+    leftType: "m"
   }
 
   componentDidMount = () => {
@@ -16,31 +14,24 @@ class App extends Component {
   }
 
   _selectDataTest = async () => {
-    const datalist = await this._selectDate(10);
-    
+    const menuList = await this._selectDate();
+    this.setState({
+      menuList
+    })
   }
 
-  _selectDate = (userCode) => {
-    return fetch(`http://localhost:7000/usercheck?usercode=${userCode}`)
-    .then(respones => console.log(JSON.parse(respones)))
-    .catch()
-  }
-
-  state = {
-    topMenu:[
-      "주문재고관리",
-      "주문내역확인",
-      "주문내역조회",
-      "로그아웃"
-    ]
+  _selectDate = () => {
+    return fetch(`http://localhost:7000/getTotalMenu?type=${this.state.leftType}`)
+    .then(respones => respones.json())
+    .then(json => json.menuList)
+    .catch(err => console.log(err))
   }
 
   render() {
+    console.log(this.state.menuList)
     return (
       <div className="App">
-        <Main title={"안녕하세요"}/>
-        {/* <Top {...this.state.topMenu}/> */}
-        <Nav title={"안녕하세요"}/>
+        <Main title={"안녕하세요"} />
       </div> 
     );
   }
