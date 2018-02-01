ea7947a78d0735d7cf80c558457c7d1754a4592b
seonhyungjo <senjing9379@gmail.com>
2018-02-01 18:20:01 +0900

    update add contentV1, V2

--- vreedu-progect/App.js
+++ vreedu-progect/App.js
@@ -62,7 +62,8 @@ export default class App extends React.Component {
           </View>
           <View style={styles.headerTextView}>
             <Text style={styles.headerText}>
-              <MaterialIcons name="email" size={15} color="#fff" /> esketch@gmail.com
+              <MaterialIcons name="email" size={15} color="#fff" />{" "}
+              esketch@gmail.com
             </Text>
             <View style={styles.iconView}>
               <Entypo
@@ -93,8 +94,9 @@ export default class App extends React.Component {
           </View>
 
           <View style={styles.headerTitleView}>
-            <Image style={styles.titleLogo}
-              source={require('./src/images/vreedu_logo.png')}
+            <Image
+              style={styles.titleLogo}
+              source={require("./src/images/vreedu_logo.png")}
             />
             <TouchableOpacity style={styles.list}>
               <MaterialIcons name="list" size={35} color="#fff" />
@@ -103,31 +105,152 @@ export default class App extends React.Component {
         </View>
 
         <ScrollView style={styles.scrollBody}>
-
           {/* <ImageBackground imageStyle={{resizeMode: 'stretch'}} source={require('./src/images/intro-bg.jpg')}>
           </ImageBackground> */}
 
           <View style={styles.bodyViewHeader}>
-            <Text>footer</Text>
+            <Text style={styles.contentTitleW}>브리드</Text>
+            <Text style={styles.contentTitleW}>
+              국제 어학 솔루션 개발 및 공급. 국제 무역 중개업을 통한 솔루션 수출
+              사업
+            </Text>
           </View>
-          <View style={styles.bodyViewHeader}>
-            <Text>footer</Text>
+
+          <View style={styles.bodyViewText}>
+            <Image
+              style={styles.contentImage}
+              source={require("./src/images/onnBookImg.png")}
+            />
+            <Text style={styles.contentTitleG}>온북(OnnBook)</Text>
+            <Text style={styles.contentDetailTitle2}>
+              온북은 스마트 포인트 리딩 앱으로 원어민 음성을 단어별 글과
+              매치되어 있어 전자화면의 글 읽기에 집중할 수 있게 해 줍니다.
+            </Text>
+
+            <View style={styles.contentDetail2}>
+              <Text style={styles.contentDetailSub2}>
+                듣기, 읽기, 말하기, 단어사전, 한글 번역, 등을 지원{" "}
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                세계 뉴스 : 미국 앵커의 원어민 음성과 기사 내용 제공
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                대중 소설 : 세계 100대 고전문학, 단편 소설{" "}
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                연설문 : 세계 유명인들의 연설문
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                청취시험 : 실제 출시 되었던 토익 등 청취 훈련 원어민 음성과
+                텍스트의 매치 리딩 원어민 음성 대비 비교 분석
+              </Text>
+            </View>
+            
           </View>
-          <View style={styles.bodyViewHeader}>
-            <Text>footer</Text>
+
+          <View style={styles.bodyViewText}>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.contentTitleG}>온북(OnnBook)</Text>
+              <Text style={styles.contentTitleW}>주요기능</Text>
+            </View>
+
+            <View style={styles.contentDetailList}>
+              <View style={styles.contentDetail}>
+                <MaterialIcons name="phone-iphone" size={50} color="#000" />
+                <Text style={styles.contentDetailTitle}>영어 듣기 평가</Text>
+                <Text style={styles.contentDetailSub}>
+                  중학교/고등학교/토익 기출문제 중심 듣기 평가 청취훈련
+                </Text>
+              </View>
+              <View style={styles.contentDetail}>
+                <MaterialIcons name="phone-iphone" size={50} color="#000" />
+                <Text style={styles.contentDetailTitle}>세계단편소설</Text>
+                <Text style={styles.contentDetailSub}>
+                  영어도서관. 세계 100대 고전문학, 단편 소설 원어민 음성과
+                  텍스트 매치 리딩
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.contentDetailList}>
+              <View style={styles.contentDetail}>
+                <MaterialIcons name="phone-iphone" size={50} color="#000" />
+                <Text style={styles.contentDetailTitle}>북마크</Text>
+                <Text style={styles.contentDetailSub}>
+                  스터디 중 북마크한 모든 위치 표시
+                </Text>
+              </View>
+              <View style={styles.contentDetail}>
+                <MaterialIcons name="phone-iphone" size={50} color="#000" />
+                <Text style={styles.contentDetailTitle}>영문 명연설</Text>
+                <Text style={styles.contentDetailSub}>
+                  세계 100대 명연설문 원문제공 및 원어민 음성과 텍스트 매치 리딩
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.contentDetailList}>
+              <View style={styles.contentDetail}>
+                <MaterialIcons name="phone-iphone" size={50} color="#000" />
+                <Text style={styles.contentDetailTitle}>뉴스, 신문 청취</Text>
+                <Text style={styles.contentDetailSub}>
+                  최신 영자신문 음성서비스 제공 미국 앵커의 음성과 기사제고,
+                  텍스트 매칭리딩
+                </Text>
+              </View>
+              <View style={styles.contentDetail}>
+                <MaterialIcons name="phone-iphone" size={50} color="#000" />
+                <Text style={styles.contentDetailTitle}>세계의 명언</Text>
+                <Text style={styles.contentDetailSub}>
+                  하루에 한중 명언으로 배우는 영어 영문 명언 원어민 음성과
+                  텍스트 매치 리딩
+                </Text>
+              </View>
+            </View>
           </View>
-          <View style={styles.bodyViewHeader}>
-            <Text>footer</Text>
+
+          <View style={styles.bodyViewText}>
+            <Image
+              style={styles.contentImage}
+              source={require("./src/images/onnBookImg.png")}
+            />
+            <Text style={styles.contentTitleG}>온북(OnnBook)</Text>
+            <Text style={styles.contentDetailTitle2}>
+              온북은 스마트 포인트 리딩 앱으로 원어민 음성을 단어별 글과
+              매치되어 있어 전자화면의 글 읽기에 집중할 수 있게 해 줍니다.
+            </Text>
+
+            <View style={styles.contentDetail2}>
+              <Text style={styles.contentDetailSub2}>
+                듣기, 읽기, 말하기, 단어사전, 한글 번역, 등을 지원{" "}
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                세계 뉴스 : 미국 앵커의 원어민 음성과 기사 내용 제공
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                대중 소설 : 세계 100대 고전문학, 단편 소설{" "}
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                연설문 : 세계 유명인들의 연설문
+              </Text>
+              <Text style={styles.contentDetailSub2}>
+                청취시험 : 실제 출시 되었던 토익 등 청취 훈련 원어민 음성과
+                텍스트의 매치 리딩 원어민 음성 대비 비교 분석
+              </Text>
+            </View>
+            
           </View>
+
           <View style={styles.bodyViewHeader}>
-            <Text>footer</Text>
+            <Text>모두의 공책</Text>
           </View>
-          <View style={styles.bodyViewHeader}>
-            <Text>footer</Text>
+
+          <View style={styles.bodyViewText}>
+            <Text>지도</Text>
           </View>
 
           <View style={styles.footer}>
-            <Text>footer</Text>
+            <Text style={styles.headerText}>footer</Text>
           </View>
         </ScrollView>
       </View>
@@ -166,8 +289,8 @@ const styles = StyleSheet.create({
   },
   titleLogo: {
     flex: 1,
-    resizeMode: 'contain',
-    marginRight: 50,
+    resizeMode: "contain",
+    marginRight: 50
   },
   iconView: {
     flex: 1,
@@ -180,13 +303,74 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(255,255,255,0.2)",
     borderRadius: 5
   },
-  bodyViewHeader:{
-    minHeight: 300,
+  bodyViewHeader: {
+    backgroundColor: "yellow",
+    alignItems: "center"
+  },
+  bodyViewText: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    backgroundColor: "#F5F7EB",
+    alignItems: "center"
+  },
+  contentTitleG: {
+    fontSize: 30,
+    color: "#51D89E",
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 20
+  },
+  contentTitleW: {
+    fontSize: 30,
+    color: "black",
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 20
+  },
+  contentDetailList: {
+    flexDirection: "row"
+  },
+  contentImage: {
+    width: 200,
+    height: 200
+  },
+  contentDetail: {
+    flex: 1,
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  contentDetailTitle: {
+    fontSize: 20,
+    fontWeight: "700"
+  },
+  contentDetailSub: {
+    fontSize: 12,
+    fontWeight: "400"
+  },
+  contentDetail2: {
+    alignItems: "flex-start",
+  },
+  contentDetailTitle2: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginLeft: 50,
+    marginRight: 50,
+    marginBottom: 20,
+    borderColor: "#fff"
+  },
+  contentDetailSub2: {
+    marginTop: 5,
+    fontSize: 14,
+    fontWeight: "200",
+    marginLeft: 30,
+    marginRight: 30
   },
   footer: {
+    flex: 1,
+    height: 70,
     backgroundColor: "#39313C"
   },
-  scrollBody: {
-    flex: 1
-  }
+  scrollBody: {}
 });
