d0aed204410876db471746880bf3757205fc78a8
seonhyungjo <senjing9379@gmail.com>
2018-02-01 15:44:08 +0900

    add header, body, footer

--- vreedu-progect/App.js
+++ vreedu-progect/App.js
@@ -1,36 +1,36 @@
 import React from "react";
-import { AppRegistry, ScrollView, Image, Text, FlatList, StyleSheet, View } from "react-native";
+import { StatusBar, AppRegistry, ScrollView, Image, Text, FlatList, StyleSheet, View, SectionList } from "react-native";
 
 export default class App extends React.Component {
+  
   constructor(props) {
     super(props);
-    this.state = { text: "" };
-  }
-
-  _onPressButton() {
-    Alert.alert("You tapped the button!");
-  }
-
-  _onLongPressButton() {
-    Alert.alert("You long-pressed the button!");
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-        />
+        <StatusBar hidden={true}/>
+
+        <View style={styles.header}>
+          <Text>footer</Text>
+        </View>
+
+        <View style={styles.body}>
+          <SectionList
+            sections={[
+              {title: 'D', data: ['Devin']},
+              {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
+        
+        <View style={styles.footer}>
+          <Text>footer</Text>
+        </View>
       </View>
     );
   }
@@ -39,11 +39,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
    flex: 1,
-   paddingTop: 22
   },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+  header: {
+    flex: 1,
+    backgroundColor: '#aaa',
   },
+  body: {
+    flex: 6,
+
+  },
+  footer: {
+    flex: 1,
+    backgroundColor:'red',
+  }
 })
