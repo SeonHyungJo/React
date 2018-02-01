ce232fedb1aea12c7d64abe3d633f34dd5c8841a
seonhyungjo <senjing9379@gmail.com>
2018-02-01 15:32:20 +0900

    add scroll view

--- vreedu-progect/App.js
+++ vreedu-progect/App.js
@@ -1,76 +1,31 @@
-import React from 'react';
-import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View  } from 'react-native';
+import React from "react";
+import { AppRegistry, ScrollView, Image, Text } from "react-native";
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = { text: "" };
   }
 
   _onPressButton() {
-    Alert.alert('You tapped the button!')
+    Alert.alert("You tapped the button!");
   }
 
   _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
+    Alert.alert("You long-pressed the button!");
   }
 
   render() {
     return (
-      <View style={styles.container}>
-
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="pink">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableNativeFeedback
-            onPress={this._onPressButton}
-            background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-
-        <TouchableWithoutFeedback
-            onPress={this._onPressButton}
-            >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long Press</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+      <ScrollView>
+        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+        <Text style={{ fontSize: 96 }}>If you like</Text>
+        <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+        <Text style={{ fontSize: 96 }}>What's the best</Text>
+        <Text style={{ fontSize: 96 }}>Framework around?</Text>
+        <Text style={{ fontSize: 80 }}>React Native</Text>
+      </ScrollView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center'
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
-})
