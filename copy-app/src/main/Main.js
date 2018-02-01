6280d5141a55e8d3011fc084bfc7443352c8fc84
seonhyungjo <senjing9379@gmail.com>
2018-02-01 14:14:29 +0900

    add toggle menu

--- copy-app/src/main/Main.js
+++ copy-app/src/main/Main.js
@@ -1,92 +1,31 @@
 import React, { Component } from "react";
 import "./main.css";
+import '../bootstrap.min.css';
+import MenuList from './menuList/menuList';
 
 export default class Main extends Component {
   render() {
-    // const { title, menuList, isPlaying } = this.props;
     return (
       <div className="mainbar">
-        <div className="page-head">
+        <div className="page-head col-sm-12 col-md-12">
           <h2 id="pageTitle" className="pull-left">
             IMHOME
           </h2>
         </div>
-        <div className="container">
-          <div id="pMenu" className="padd">
+        <div className="container col-sm-12 col-md-6">
+          <div id="pMenu">
             <div className="pRowMenu">
-              <div id="" className="pButton seleted open">
-                <b />
-                <span className="menuRight">
-                  <i className="fa fa-chevron-down fa-2x" />
-                </span>
-                <span className="menuSubTitle">
-                  survived not only five centuries but the leap
-                </span>
-              </div>
-              <div id="" className="pButton">
-                <b />
-                <span className="menuRight">
-                  <i className="fa fa-chevron-down fa-2x" />
-                </span>
-                <span className="menuSubTitle">
-                  survived not only five centuries but the leap
-                </span>
-              </div>
-
-              <table className="pTable">
-                <colgroup>
-                  <col width="*" />
-                  <col width="15%" />
-                  <col width="15%" />
-                  <col width="20%" />
-                  <col width="15%" />
-                </colgroup>
-                <thead>
-                  <tr>
-                    <th>품목</th>
-                    <th>단위</th>
-                    <th>가격</th>
-                    <th>재고량</th>
-                    <th>주문</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="List_${mainMenu.fCode}">
-                    <td id="${mainMenu.fCode}_Name" />
-                    <td />
-                    <td id="${mainMenu.fCode}_Cost" />
-                    <td />
-                    <td className="pMount">
-                      <div className="upDownDiv">
-                        <span
-                          className="upDownBtn"
-                          onclick="javascript:mountClick('down', '${mainMenu.fCode}');"
-                        >
-                          -
-                        </span>
-                        <input
-                          className="${mainMenu.fCode}_OrderNum"
-                          type="number"
-                          placeholder="0"
-                          max="${fRestMount}"
-                          onkeyup="javascript:pressNumber('${mainMenu.fCode}', '${mainMenu.fRestMount}')"
-                        />
-                        <span
-                          className="upDownBtn"
-                          onclick="javascript:mountClick('up','${mainMenu.fCode}', '${mainMenu.fRestMount}');"
-                        >
-                          +
-                        </span>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+              <MenuList/>
+              <MenuList/>
+              <MenuList/>
+              <MenuList/>
+              <MenuList/>
+              <MenuList/>
             </div>
           </div>
         </div>
 
-        <div className="container">
+        <div className="container col-sm-12 col-md-6">
           <div className="orderContainer">
             <div className="order">
               <div className="orderHead">
---
+++ copy-app/src/main/menuList/index.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators as tomatoActions } from "../reducer";
+import MenuList from "./menuList";
+
+
+// reducer에서 가져오면 변수를 가져오는 func
+function mapStateToProps(state) {
+  const { isPlaying, elapsedTime, timerDuration } = state;
+
+  return {
+    isPlaying,
+    elapsedTime,
+    timerDuration
+  };
+}
+
+// reducer에서 가져오면 행위를 가져오는 func
+function mapDispatchToProps(dispatch) {
+  return {
+    startTimer: bindActionCreators(tomatoActions.startTimer, dispatch),
+    restartTimer: bindActionCreators(tomatoActions.restartTimer, dispatch),
+    addSecond: bindActionCreators(tomatoActions.addSecond, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MenuList);
